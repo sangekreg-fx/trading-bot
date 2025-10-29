79fc57041c98fa39b6a95d88a038c0708203d479
sangekreg-fx <barjonabyron@gmail.com>
2025-10-29 16:30:58 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,20 @@
+# config.py
+
+# === API KEYS ===
+TWELVE_DATA_API_KEY = "d3732b20faae4551b72df75bf3d6ffd1"
+FINNHUB_API_KEY = "d40v3ahr01qhkm8b1lrgd40v3ahr01qhkm8b1ls0"
+
+# === TWILIO WHATSAPP ===
+TWILIO_SID = "ACc71a1cfeda69b90707a8fdf0806da159"
+TWILIO_AUTH_TOKEN = "08888e163f2820f54e3953d8a7d6c27f"
+TWILIO_WHATSAPP_FROM = "whatsapp:+14155238886"
+WHATSAPP_TO = "whatsapp:+256752721686"
+
+# === GMAIL ALERTS ===
+GMAIL_USER = "jacobtimba0@gmail.com"
+GMAIL_PASSWORD = "BoB5/01/2007"
+GMAIL_TO = "jacobtimba0@gmail.com"
+
+# === GOOGLE SHEETS ===
+GOOGLE_SHEET_NAME = "Trading Signals"
+GOOGLE_CREDENTIALS_FILE = "credentials.json"
